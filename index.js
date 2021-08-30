762ebea254512cfd172aa93982e2ac9a98c36ef9
Sumidha KP <kumari.sumidha.123@gmail.com>
2021-08-30 16:57:18 +0530

    selecting elements for DOM

---
+++ index.js
@@ -0,0 +1,15 @@
+// Step 1 selecting all the elements
+var initialPrice=document.getElementById("initial-price");
+var currentPrice=document.getElementById("current-price");
+var stockQuantity=document.getElementById("stock-quantity");
+var submitBtn=document.getElementById("submit-btn");
+var outputBox=document.getElementById("output-box");
+
+// other way to select elements
+// var initialPrice=document.querySelector("#initial-price");
+// var currentPrice=document.querySelector("#current-price");
+// var stockQuantity=document.querySelector("#stock-quantity");
+// var submitBtn=document.querySelector("#submit-btn");
+// var outputBox=document.querySelector("#output-box");
+
+
